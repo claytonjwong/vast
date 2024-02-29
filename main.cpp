2011e55547fdda1164d7e43f7fe1354edff82054
claytonjwong <claytonjwong@gmail.com>
2024-02-29 06:11:58 -0700

    cleanup main controller code with a run() function

--- main.cpp
+++ main.cpp
@@ -38,26 +38,32 @@ std::tuple<int, int, int, int> getArgs(int argc, const char* argv[]) {
         parser.print_help();
         exit(0);
     }
-    auto N = parser.get<int>("trucks");  // TODO: consider upper limit assertion, since we spawn a thread for each truck
-    auto M = parser.get<int>("queues");  // TODO: consider upper limit assertion, since we spawn a thread for each queue
-    auto TIME_RATIO = parser.get<double>("ratio");
-    auto SIMULATION_HOURS = parser.get<int>("duration");
-    assert(0 < N);
-    assert(0 < M);
+    const auto TRUCK_CNT = parser.get<int>("trucks");
+    const auto QUEUE_CNT = parser.get<int>("queues");
+    const auto TIME_RATIO = parser.get<double>("ratio");
+    const auto SIMULATION_HOURS = parser.get<int>("duration");
+    assert(0 < TRUCK_CNT && TRUCK_CNT <= 1000);
+    assert(0 < QUEUE_CNT && QUEUE_CNT <= 1000);
     assert(0 < TIME_RATIO);
     assert(0 < SIMULATION_HOURS);
-    return std::make_tuple(N, M, TIME_RATIO, SIMULATION_HOURS);
+    return std::make_tuple(TRUCK_CNT, QUEUE_CNT, TIME_RATIO, SIMULATION_HOURS);
 }
 
-int main(int argc, const char* argv[]) {
-    auto [N, M, TIME_RATIO, SIMULATION_HOURS] = getArgs(argc, argv);
+void run(int argc, const char* argv[]) {
+    //
+    // simulation input params
+    //
+    auto [TRUCK_CNT, QUEUE_CNT, TIME_RATIO, SIMULATION_HOURS] = getArgs(argc, argv);
     logger::log(__LINE__, __FILE__, "🎮 starting simulation with ",
-        N, " truck(s) and ", M, " unloading queue(s) using time warp ratio ", TIME_RATIO,
-        " for duration ", SIMULATION_HOURS, " hours");
-    auto simulation_duration = std::chrono::seconds(SIMULATION_HOURS * 60 * 60) / TIME_RATIO;  // FIXME? multiply by 60 * 60 to convert hours to seconds
+        TRUCK_CNT, " truck(s) and ", QUEUE_CNT, " unloading queue(s) using time warp ratio ",
+        TIME_RATIO, " for duration ", SIMULATION_HOURS, " hours");
+    auto simulation_duration = std::chrono::seconds(SIMULATION_HOURS * 60 * 60) / TIME_RATIO;  // multiply by 60 * 60 to convert hours to seconds
     logger::log(__LINE__, __FILE__, "🎮 simulation duration ", simulation_duration.count(), " seconds (our world's real-time)");
-    std::vector<joining_thread> truck_threads;
-    storage_station station(N, M, SIMULATION_HOURS);
+
+    //
+    // servers
+    //
+    storage_station station(TRUCK_CNT, M, SIMULATION_HOURS);
     threadsafe_queue<std::shared_ptr<Truck>> unload_queue;  // Trucks push themselves onto this emphemeral queue to the storage station
     auto unload_queue_work = [&] {
         logger::log(__LINE__, __FILE__, "🗑️ running detached thread for unload_queue_work 🔨");
@@ -69,6 +75,13 @@ int main(int argc, const char* argv[]) {
         logger::log(__LINE__, __FILE__, "🎮 Error: running detached thread for unload_queue_work should NOT reach this code leg! 🛑");
         assert(false && "🎮 Error: unreachable code!  The unload queue must remain running until all trucks have finished! 🛑");
     };
+    joining_thread unload_queue_thread{ unload_queue_work };
+    unload_queue_thread.detach();
+
+    //
+    // clients
+    //
+    std::vector<joining_thread> truck_threads;
     auto now = std::chrono::steady_clock::now;
     auto start = now();
     auto delta = [&now](auto start) { return std::chrono::duration<double>(now() - start); }; // seconds
@@ -80,12 +93,18 @@ int main(int argc, const char* argv[]) {
         }
         logger::log(__LINE__, __FILE__, "🎮 simulation duration acquired, 🚚 truck[", i, "] work done 🎉");
     };
-    joining_thread unload_queue_thread{ unload_queue_work };
-    unload_queue_thread.detach();
-    generate_n(back_inserter(truck_threads), N, [&truck_work, i = -1]() mutable { return joining_thread{ truck_work, ++i }; });
+    generate_n(back_inserter(truck_threads), TRUCK_CNT, [&truck_work, i = -1]() mutable { return joining_thread{ truck_work, ++i }; });
     for (auto& thread: truck_threads) {
         thread.join();
     }
+
+    //
+    // metrics
+    //
     logger::log(__LINE__, __FILE__, station.get_metrics());
+}
+
+int main(int argc, const char* argv[]) {
+    run(argc, argv);
     return 0;
 }
