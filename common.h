b7aa57b939e0e259427075655d17f28f9474c45c
claytonjwong <claytonjwong@gmail.com>
2024-02-29 16:25:24 -0700

    fix typo: 'mutable' -> 'mutual'

--- common.h
+++ common.h
@@ -3,7 +3,7 @@
 #include <memory>
 #include "threadsafe_queue.h"
 
-class Truck; // forward declaration so truck can use this header file without this header file needing to include "truck.h" (ie. avoid mutable dependence since it doesn't compile!)
+class Truck; // forward declaration so truck can use this header file without this header file needing to include "truck.h" (ie. avoid mutual dependence since it doesn't compile!)
 using TruckPtr = std::shared_ptr<Truck>;
 using Queue = threadsafe_queue<TruckPtr>;
 using QueuePtr = std::shared_ptr<Queue>;
