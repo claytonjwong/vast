f615bdf5dc986737c80eca2176e08bdb84c0510c
claytonjwong <claytonjwong@gmail.com>
2024-02-29 12:17:56 -0700

    remove unused exception

--- threadsafe_queue.h
+++ threadsafe_queue.h
@@ -1,16 +1,8 @@
 #pragma once
 
-#include <exception>
 #include <condition_variable>
 #include <mutex>
 #include <queue>
-
-struct empty_queue : std::exception {
-    const char* what() const throw() {
-        return "queue is empty";
-    }
-};
-
 template <typename T>
 class threadsafe_queue {
     std::queue<T> q;
